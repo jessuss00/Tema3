336d62bf1080904aff685e5defb88e84818168ff
jessuss00 <jesusmadro23@gmail.com>
2024-10-15 13:43:38 +0200

    nv

---
+++ Tema3/src/Tema3_jmg/Diapostiva8.java
@@ -0,0 +1,17 @@
+package Tema3_jmg;
+
+
+
+public class Diapostiva8 {
+
+	public static void main(String[] args) {
+		
+	int n=0;
+	while(n <=50) {
+		System.out.println(n);
+		n=n+2;
+	}
+
+	}
+
+}
