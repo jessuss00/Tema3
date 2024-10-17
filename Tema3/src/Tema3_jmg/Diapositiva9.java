35eb866a3caafc6b7173e6e6166f747c494729e1
jessuss00 <jesusmadro23@gmail.com>
2024-10-17 11:00:54 +0200

    nuevo

---
+++ Tema3/src/Tema3_jmg/Diapositiva9.java
@@ -0,0 +1,24 @@
+package Tema3_jmg;
+
+import java.util.Scanner;
+
+public class Diapositiva9 {
+
+	public static void main(String[] args) {
+		Scanner ac = new Scanner(System.in);
+		
+		int n=1;
+		int suma=0;
+		while(n<=10) {
+			System.out.println("Dame un valor");
+			int beti = ac.nextInt();
+			suma = suma +beti;
+			n++;
+		}
+		double promedio = 1.0* suma /n;
+		System.out.println("La suma de tus numeros es: "+ suma);
+		System.out.printf("El promedio de tus numeros es %.2f ", promedio);
+
+	}
+
+}
